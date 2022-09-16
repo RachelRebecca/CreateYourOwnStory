c838b0a2827d374ea523d920401846ef545a338a
RachelRebecca <rachelrebecca67@gmail.com>
2022-09-15 21:10:01 -0400

    got headlines to work a bit better, reorganized files
    need to get ending a page to work and also traversing left and right properly depending on the headline

--- js/FillInStory.js
+++ js/FillInStory.js
@@ -1,5 +1,66 @@
-function insertNewTreeElement(pageBeforeHeadline, headline, text)
-{
+function doOnSubmit() {
+    let element = document.getElementById("thisPageHeadline");
+    let index = element.selectedIndex;
+    let selectedOption = element.options[index];
+    let thisPageHeadline = selectedOption.label;
+
+    if (document.getElementById("doesThisContinue").checked) {
+        let headline1 = prompt("Enter the headline for the first of the next two pages: ");
+        let headline2 = prompt("Enter the headline for the second of the next two pages: ");
+
+        console.log(headline1 + " " + headline2);
+
+        let headlines = JSON.parse(sessionStorage.getItem("headlines"));
+
+        if (headlines === null) {
+            headlines = [headline1, headline2];
+        } else {
+
+            headlines.push(headline1);
+            headlines.push(headline2);
+
+            const index = headlines.indexOf(thisPageHeadline);
+            if (index > -1) { // only splice array when item is found
+                headlines.splice(index, 1); // 2nd parameter means remove one item only
+            }
+
+        }
+
+        sessionStorage.setItem("headlines", JSON.stringify(headlines));
+
+        insertNewTreeElement(previousHeadline, thisPageHeadline, document.getElementById("text").value);
+
+        sessionStorage.setItem("previousHeadline", thisPageHeadline);
+    } else {
+        insertNewTreeElement(previousHeadline, thisPageHeadline, document.getElementById("text").value);
+        insertNewTreeElement(thisPageHeadline, tree.END, "");
+    }
+
+    let nodes = tree.getAllNodes();
+
+    function compare(a, b) {
+        const pathA = a.nodePath.split(" ");
+        const pathB = b.nodePath.split(" ");
+
+        let comparison = 0;
+        if (pathA.length > pathB.length) {
+            comparison = 1;
+        } else if (pathA.length < pathB.length) {
+            comparison = -1;
+        }
+        return comparison;
+    }
+
+    nodes = nodes.sort(compare);
+
+    console.log(nodes);
+
+    sessionStorage.setItem("nodes", JSON.stringify(nodes));
+
+    showStory();
+}
+
+function insertNewTreeElement(pageBeforeHeadline, headline, text) {
     console.log("Page before headline: " + pageBeforeHeadline + " headline: " + headline + " text: " + text);
 
     if (pageBeforeHeadline === "") {
