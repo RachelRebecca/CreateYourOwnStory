aeed919a9d2cc544ca65030f832f7fa9a3b730f8
RachelRebecca <rachelrebecca67@gmail.com>
2022-09-24 21:26:41 -0400

    added bootstrap https://getbootstrap.com/ and normalize.css
    Updated title and nav for each html page
    
    moved setting up script to new javascript file

---
+++ js/SetUpStory.js
@@ -0,0 +1,45 @@
+let nodes;
+
+if (sessionStorage.getItem('nodes') !== null) {
+    nodes = JSON.parse(sessionStorage.getItem('nodes'));
+    console.log(nodes);
+
+    for (let i = 0; i < nodes.length; i++) {
+        tree.insert(nodes[i].nodePath, nodes[i].headline, nodes[i].text);
+    }
+}
+
+let root;
+if (sessionStorage.getItem('root') === null) {
+    root = prompt("Insert headline for the first, root page");
+    sessionStorage.setItem('root', root);
+} else {
+    root = sessionStorage.getItem('root');
+}
+
+let options = [];
+if (tree.isEmpty()) {
+    options.push(root);
+} else {
+    let dropdownValues = JSON.parse(sessionStorage.getItem('dropdownValues'));
+
+    if (dropdownValues !== null) {
+        for (let i = 0; i < dropdownValues.length; i++) {
+            if (dropdownValues[i] !== tree.END) {
+                options.push(dropdownValues[i]);
+            }
+        }
+    }
+}
+
+if (options.length === 0) {
+    alert("You have written finished writing all of your stories!!");
+    document.getElementById("createStory").style.display = "none";
+}
+
+for (let i = 0; i < options.length; i++) {
+    let option = document.createElement("option");
+    option.value = i + "";
+    option.label = options[i];
+    document.getElementById("thisPageHeadline").append(option);
+}
