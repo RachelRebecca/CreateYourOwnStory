379bf3d9bfbf7acd51eee7e3787ea913809e7eb1
RachelRebecca <rachelrebecca67@gmail.com>
2022-09-29 08:30:39 -0400

    added footer
    Note: must make slideshow responsive

---
+++ js/footer.js
@@ -0,0 +1,6 @@
+let copyright = "&copy;";
+let font = " style = 'font-family: Baskerville, serif; font-style: italic; text-align: center;'";
+document.write(
+    "<hr>" +
+    "<h2" + font + ">" + copyright + " Create Your Own Story</h2>" +
+    "<h3" + font + ">by Nemesure and Co.</h3>")
