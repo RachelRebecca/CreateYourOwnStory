25714ba7912401dcfae308894c1679b21f486215
RachelRebecca <rachelrebecca67@gmail.com>
2022-09-30 09:36:05 -0400

    made footer from bootstrap

--- js/footer.js
+++
@@ -1,6 +0,0 @@
-let copyright = "&copy;";
-let font = " style = 'font-family: Baskerville, serif; font-style: italic; text-align: center;'";
-document.write(
-    "<hr>" +
-    "<h2" + font + ">" + copyright + " Create Your Own Story</h2>" +
-    "<h3" + font + ">by Nemesure and Co.</h3>")
